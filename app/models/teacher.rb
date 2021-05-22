284d710de16af7770716eae092ae480e19b3509b
alexandre <alecarv98@gmail.com>
2021-05-22 17:35:17 -0300

    imagem padrao adicionada

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,14 @@
 class Teacher < ApplicationRecord
-    has_one_attached :profile_picture
-
     validates :name, :email, presence: { message: 'Cadastre todas as informações' }
     validates :email, uniqueness: { message: 'O email já está em uso' }
+
+    has_one_attached :profile_picture
+
+    before_create :attach_default_profile_picture
+
+    def attach_default_profile_picture
+        return if profile_picture.attached?
+            profile_picture.attach(io: File.open('spec/fixtures/default.png'),
+                                   filename: 'default.png')
+    end
 end
