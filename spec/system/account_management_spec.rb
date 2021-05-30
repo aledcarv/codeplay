2696b102acf5edc995d36756211dc5440db378ee
alexandre <alecarv98@gmail.com>
2021-05-30 16:55:23 -0300

    usuário entra e sai

--- spec/system/account_management_spec.rb
+++ spec/system/account_management_spec.rb
@@ -52,4 +52,43 @@ describe 'account management' do
             expect(page).to have_text('não é igual a Senha')
         end
     end
+
+    context 'sign in' do
+        it 'with email and password' do
+            User.create!(email: 'pessoa.cadastro@code.com', password: '012345')
+
+            visit root_path
+            click_on 'Entrar'
+
+            fill_in 'Email', with: 'pessoa.cadastro@code.com'
+            fill_in 'Senha', with: '012345'
+            within 'form' do
+                click_on 'Entrar'
+            end
+
+            expect(current_path).to eq(root_path)
+            expect(page).to have_text('Login efetuado com sucesso')
+            expect(page).to have_text('pessoa.cadastro@code.com')
+            expect(page).to have_text('Sair')
+            expect(page).to_not have_text('Registrar-me')
+            expect(page).to_not have_text('Entrar')
+        end
+    end
+
+    context 'log out' do
+        it 'successfully' do
+            user = User.create!(email: 'pessoa.cadastro@code.com', password: '012345')
+
+            login_as user, scope: :user
+            visit root_path
+            click_on 'Sair'
+
+            expect(current_path).to eq(root_path)
+            expect(page).to have_text('Saiu com sucesso')
+            expect(page).to_not have_text('pessoa.cadastro@code.com')
+            expect(page).to_not have_text('Sair')
+            expect(page).to have_text('Registrar-me')
+            expect(page).to have_text('Entrar')
+        end
+    end
 end
