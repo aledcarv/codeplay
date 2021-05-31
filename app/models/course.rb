149cca4316ecd201366f74acb19866fb89c518ec
alexandre <alecarv98@gmail.com>
2021-05-31 12:00:13 -0300

    compras

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
     belongs_to :teacher
     has_many :lessons
+    has_many :users, through: :enrollments
 
     validates :name, :code, :price, presence: true
     validates :code, uniqueness: true
--- spec/system/student_view_available_courses_spec.rb
+++ spec/system/student_view_available_courses_spec.rb
@@ -63,7 +63,7 @@ describe 'student view available courses' do
                                           code: 'GEOCURSO', price: 30,
                                           enrollment_deadline: 1.month.from_now, teacher: teacher)
         other_course = Course.create!(name: 'Sociologia', description: 'Curso de sociologia',
-                                      code: 'SOCIOCURSO', price: 30,
+                                      code: 'SOCIOCURSO', price: 25,
                                       enrollment_deadline: 1.month.from_now, teacher: teacher)
 
         login_as user, scope: :user
@@ -71,9 +71,33 @@ describe 'student view available courses' do
         click_on 'Geografia'
         click_on 'Comprar'
 
-        expect(page).to have_content('Curso comprado com sucesso')
         expect(current_path).to eq(my_enroll_courses_path)
+        expect(page).to have_content('Curso comprado com sucesso')
         expect(page).to have_content('Geografia')
+        expect(page).to have_content('R$ 30,00')
         expect(page).to_not have_content('Sociologia')
+        expect(page).to_not have_content('R$ 25,00')
+    end
+
+    it 'and can not buy a course twice' do
+        user = User.create!(email: 'jane.doe@code.com', password: '012345')
+        teacher = Teacher.create!(name: 'Gonzaga',
+                                  email: 'gonzaga.prof@code.com')
+        available_course = Course.create!(name: 'Geografia', description: 'Curso de geografia',
+                                          code: 'GEOCURSO', price: 30,
+                                          enrollment_deadline: 1.month.from_now, teacher: teacher)
+        Lesson.create!(name: 'Efeito estufa', content: 'Aula sobre efeito estufa', course: available_course)
+        Enrollment.create!(user: user, course: available_course)
+
+        login_as user, scope: :user
+        visit root_path
+        click_on 'Geografia'
+
+        expect(page).to_not have_link('Comprar')
+        expect(page).to have_link('Efeito estufa')
+    end
+
+    xit 'without enrollment can not view lesson link' do
+        
     end
 end
